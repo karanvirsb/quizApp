0d81962f922d8a4562a4259ece33e27c47290343
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-19 12:56:37 -0400

    created a quick promise function

--- src/model/Data.js
+++ src/model/Data.js
@@ -1,1 +1,12 @@
 const url = "https://opentdb.com/api.php?"; 
+
+function getQuiz(amount, category, type, difficulty){
+    const resp = fetch(url + 'amount=' + amount);
+    return new Promise((resolve, reject) => {
+        if(resp){
+            return resolve(resp.json)
+        } else {
+            reject(`Sorry no quiz is available`); 
+        }
+    })
+}
