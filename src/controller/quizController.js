1bcd0cad2b592f28fdfbd2a07745af6aad00bd1d
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-19 13:29:02 -0400

    when the submit form btn is clicked then it will retrieve the quiz

--- src/controller/quizController.js
+++ src/controller/quizController.js
@@ -1,7 +1,13 @@
-const amount = document.querySelector('#number_of_questions__input');
-const category = document.querySelector('#cateogry__input');
-const difficulty = document.querySelector('#difficulty__input');
-const type = document.querySelector('#type__input');
+const begin_btn = document.querySelector('#form_submit');
+
+begin_btn.addEventListener('click', () => {
+    const amount = document.querySelector('#number_of_questions__input');
+    const category = document.querySelector('#cateogry__input');
+    const difficulty = document.querySelector('#difficulty__input');
+    const type = document.querySelector('#type__input');
+
+    retrieveQuiz(amount, category, difficulty, type); 
+}); 
 
 function retrieveQuiz(amount, category, difficulty, type){
     return getData(amount, category, type, difficulty)
