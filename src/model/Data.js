b28fc4232363996933f82fafe775c90e84487a41
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-19 12:59:26 -0400

    added an async function to get data

--- src/model/Data.js
+++ src/model/Data.js
@@ -1,5 +1,14 @@
 const url = "https://opentdb.com/api.php?"; 
 
+const getData = async (amount, category, type, difficulty) =>{
+    try {
+        const quiz = await getQuiz(amount); 
+        return quiz;
+    } catch (error) {
+        console.log(error); 
+    }
+}
+
 function getQuiz(amount, category, type, difficulty){
     const resp = fetch(url + 'amount=' + amount);
     return new Promise((resolve, reject) => {
