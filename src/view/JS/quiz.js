5a80a5b6021e3e7791e19eeff3344bc8bdcc8af7
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-20 12:00:56 -0400

    created getting answer, question and next array

--- src/view/JS/quiz.js
+++ src/view/JS/quiz.js
@@ -1,5 +1,24 @@
 class quiz {
-    constructor(arr){
-        this.quizArr = arr; 
+    constructor(arr, length){
+        this.quizArr = arr;
+        this.counter = 0; 
+        this.quizLength = length; 
+    }
+
+    get getAnswer(quizArr){
+        return quizArr.correct_answer; 
+    }
+
+    get question(quizArr){
+        return quizArr.question;
+    }
+
+    get nthQuizResult(){
+        if(this.counter < this.quizLength){
+            nextQuiz = this.quizArr[this.counter];
+            this.counter++; 
+            return nextQuiz;  
+        }
+        return; 
     }
 }
