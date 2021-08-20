f2c70636ec5e9eb40cf2489be9847536ec172700
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-20 09:56:28 -0400

    added .value to the selectors

--- src/controller/quizController.js
+++ src/controller/quizController.js
@@ -2,12 +2,15 @@ const begin_btn = document.querySelector('#form_submit');
 let quizJson = []; 
 
 begin_btn.addEventListener('click', () => {
-    const amount = document.querySelector('#number_of_questions__input');
-    const category = document.querySelector('#cateogry__input');
-    const difficulty = document.querySelector('#difficulty__input');
-    const type = document.querySelector('#type__input');
+    const amount = document.querySelector('#number_of_questions__input').value;
+    const category = document.querySelector('#category__input').value;
+    const difficulty = document.querySelector('#difficulty__input').value;
+    const type = document.querySelector('#type__input').value;
 
-    quizJson = retrieveQuiz(amount, category, difficulty, type); 
+    console.log(amount, category, difficulty, type);
+
+    // quizJson = retrieveQuiz(amount, category, difficulty, type);
+    // console.log(quizJson);  
 }); 
 
 function retrieveQuiz(amount, category, difficulty, type){
