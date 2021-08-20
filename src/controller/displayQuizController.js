bcbfcb889f5e619961c2d7f7d191a46b09dcaf2a
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-20 13:45:16 -0400

    query selected elements and created optioncreator func

--- src/controller/displayQuizController.js
+++ src/controller/displayQuizController.js
@@ -1,4 +1,13 @@
+const questionAmount = document.querySelector('.quiz__total');
+const questionRemaining = document.querySelector('.quiz__remaining');
+const question = document.querySelector('.quiz__body--question');
+const optionsDiv = document.querySelector(',quiz__body--container');
+
+
 function displayQuiz(quizArr){
     const quiz = new Quiz(quizArr); 
+}
+
+function optionCreator(questionOption){
     
 }
