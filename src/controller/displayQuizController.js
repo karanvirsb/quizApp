c43682725f52343dfa329d2864083c30bcdbc9fc
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-20 13:52:42 -0400

    created the optionCreator and createLetter

--- src/controller/displayQuizController.js
+++ src/controller/displayQuizController.js
@@ -2,12 +2,29 @@ const questionAmount = document.querySelector('.quiz__total');
 const questionRemaining = document.querySelector('.quiz__remaining');
 const question = document.querySelector('.quiz__body--question');
 const optionsDiv = document.querySelector(',quiz__body--container');
-
+let letter = 97; 
 
 function displayQuiz(quizArr){
     const quiz = new Quiz(quizArr); 
 }
 
-function optionCreator(questionOption){
-    
+function optionCreator(question_option){
+    let div = document.createElement('div');
+    div.classList.add('options');
+
+    let span = "" 
+    createLetter();
+
+    let option_answer = document.createElement('p');
+    option_answer.classList.add('options__answer');
+    option_answer.innerHTML = question_option; 
+
+    div.append(span); 
+    div.append(option_answer);
+}
+
+function createLetter(char) {
+    let span = document.createElement('span');
+    span.classList.add('letters');
+    return span;
 }
