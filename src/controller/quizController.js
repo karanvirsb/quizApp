de59ef55208e6df9619bfedcab834910192b31b1
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-20 10:59:49 -0400

    added preventDefault from refreshing the page

--- src/controller/quizController.js
+++ src/controller/quizController.js
@@ -1,16 +1,18 @@
 const begin_btn = document.querySelector('#form_submit');
 let quizJson = []; 
 
-begin_btn.addEventListener('click', () => {
+begin_btn.addEventListener('click', e => {
+    e.preventDefault();
     const amount = document.querySelector('#number_of_questions__input').value;
     const category = document.querySelector('#category__input').value;
     const difficulty = document.querySelector('#difficulty__input').value;
     const type = document.querySelector('#type__input').value;
 
-    quizJson = retrieveQuiz(amount, category, difficulty, type);
-    console.log(quizJson);  
+    quizJson = retrieveQuiz(amount, category, type, difficulty);
 }); 
 
-function retrieveQuiz(amount, category, difficulty, type){
-    return getData(amount, category, type, difficulty);
+function retrieveQuiz(amount, category, type, difficulty) {
+    getData(+amount, category, type, difficulty).then((data) => {
+       return data.results;
+    }).catch((err) => console.log(err));
 }
