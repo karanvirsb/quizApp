0aac5a47a8a3252feee3627211b5eb9507fbe862
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-23 13:11:56 -0400

    created a function that will get the users answer

--- src/controller/answerController.js
+++ src/controller/answerController.js
@@ -1,11 +1,23 @@
 const correct = 0; 
 
 function isItCorrect(answer, userAnswer){
-    if(answer === userAnswer){
+    if(answer === userAnswer()){
         correct++; 
     }
 }
 
 function questions_correct(){
     return correct; 
+}
+
+function getUserAnswer(){
+    const radio_btns = document.querySelectorAll('.radio__btn');
+    let sibling = ''; 
+    radio_btns.forEach(elem => {
+        if(elem.checked){
+            sibling = elem.nextElementSibling; 
+        }
+    });
+
+    return sibling.querySelector('.options_answer').innerHTML; 
 }
