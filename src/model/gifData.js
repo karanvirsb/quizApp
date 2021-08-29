057cda684d3f816e57c1dc790174d0d79c2d3f59
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-29 09:58:27 -0400

    added a new model that will get back data

---
+++ src/model/gifData.js
@@ -0,0 +1,17 @@
+const GIF_API_KEY = process.env.GIF_API_KEY; 
+const limit = 5; 
+const rating = 'g'
+let q = ''; 
+const gif_url =  `api.giphy.com/v1/gifs/search?api_key=${GIF_API_KEY}&q=${q}&limit=${limit}&rating=${rating}`;
+
+const getGif = async (gifInfo) => {
+    q = gifInfo;
+    try{
+        const gifs = await fetch(gif_url); 
+        const gifsJson = await gifs.json();
+        return gifsJson;  
+    } catch(err){
+        console.log(err); 
+    }
+    
+}
