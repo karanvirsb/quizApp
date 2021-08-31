adc13ae7dc9665cdc94959c6f1994fe38504e768
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-31 15:46:51 -0400

    if the element isnt in header then turn off the display

--- src/view/JS/index.js
+++ src/view/JS/index.js
@@ -1,6 +1,13 @@
+const header = document.querySelector('.header')
 const menu_btn = document.querySelector('.header__menu');
 const nav_links = document.querySelector('.nav__links');
 
 menu_btn.addEventListener('click', ()=>{
     nav_links.style.display = 'flex'; 
+});
+
+document.addEventListener('click', e => {
+    if(!header.contains(e.target)){
+        nav_links.style.display = 'none'; 
+    }
 });
