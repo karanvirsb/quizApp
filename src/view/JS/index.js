74fbdac5c5d94c359d47b12d624b62c4ea08838c
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-31 15:37:47 -0400

    added open menu

--- src/view/JS/index.js
+++ src/view/JS/index.js
@@ -1,5 +1,6 @@
 const menu_btn = document.querySelector('.header__menu');
+const nav_links = document.querySelector('.nav__links');
 
 menu_btn.addEventListener('click', ()=>{
-    
-})
+    nav_links.style.display = 'flex'; 
+});
