ffc15c1c4585c9832cb555db451027d604b986eb
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-31 15:47:40 -0400

    menu style to none

--- src/view/JS/index.js
+++ src/view/JS/index.js
@@ -4,10 +4,12 @@ const nav_links = document.querySelector('.nav__links');
 
 menu_btn.addEventListener('click', ()=>{
     nav_links.style.display = 'flex'; 
+    menu_btn.style.display = 'none';
 });
 
 document.addEventListener('click', e => {
     if(!header.contains(e.target)){
         nav_links.style.display = 'none'; 
+        menu_btn.style.display = '';
     }
 });
