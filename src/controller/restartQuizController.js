33e20eae33b8fdd5869c796bbe234b3dd1a8f080
Karavir Boparai <Karanvirboparai123@gmail.com>
2021-08-31 10:51:07 -0400

    forgot to set index

--- src/controller/restartQuizController.js
+++ src/controller/restartQuizController.js
@@ -1,7 +1,9 @@
-export function backToDefault(arr){
+function backToDefault(arr){
     for(let i = 0; i < arr.length; i++){
-        arr.picked =  0
-        arr.done = false; 
-        arr.was_it_correct = " ";  
+        arr[i].picked =  0
+        arr[i].done = false; 
+        arr[i].was_it_correct = " ";  
     }
+
+    return arr; 
 }
